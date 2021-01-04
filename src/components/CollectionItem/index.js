1c4d76ed7d476019742f1b5735aea599f0060ed1
sukrutd <sukrut.dd@gmail.com>
2021-01-04 10:05:26 +0530

    Updated cart utils to group multiple items

--- src/components/CollectionItem/index.js
+++ src/components/CollectionItem/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CustomButton from '../CustomButton';
+import { addItem } from '../../redux/cart/cart.actions';
 import './styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+	const { name, price, imageUrl } = item;
 	return (
 		<div className='collection-item'>
 			<div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,9 +13,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
 				<span className='name'>{name}</span>
 				<span className='price'>{price}</span>
 			</div>
-			<CustomButton className='inverted'>ADD TO CART</CustomButton>
+			<CustomButton className='inverted' onClick={() => addItem(item)}>
+				ADD TO CART
+			</CustomButton>
 		</div>
 	);
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+	addItem: (item) => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { CartActionTypes } from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const initialState = {
 	hidden: true,
@@ -9,6 +10,9 @@ const cartReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case CartActionTypes.TOGGLE_CART_HIDDEN:
 			return { ...state, hidden: !state.hidden };
+
+		case CartActionTypes.ADD_ITEM:
+			return { ...state, cartItems: addItemToCart(state.cartItems, action.payload) };
 
 		default:
 			return state;
